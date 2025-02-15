42194cd7dc374fbcc0d4829cf0bc58d87d9d4691
shahnoor <shahnoork109@gmail.com>
2025-02-16 00:50:09 +0530

    basics of react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,15 @@
+import Chai from './Chai'
+
+
+function App() {
+  return (
+    <>
+    <Chai />
+    <h1>chai aur react</h1>
+    <p>i have made a simple react app with the help of hitesh sir, i have deleted some files and then run this app with the help of  package.json file </p>
+    </>
+    
+  );
+}
+
+export default App;
